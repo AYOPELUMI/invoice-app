3774cf4aae900ebfeef7cb2a0e91c982784c70f0
unknown <ayopelumi2014@gmail.com>
2023-10-26 18:12:40 +0100

    updated features but not completed

--- src/InvoiceApp/SideMenu/userMenu/userMenu.jsx
+++ src/InvoiceApp/SideMenu/userMenu/userMenu.jsx
@@ -1,13 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useContext } from 'react';
 import "./style.scss"
-import{BiSolidChevronLeft} from "react-icons/bi"
+import{BiSolidChevronLeft, BiLogOut} from "react-icons/bi"
 import PropTypes from 'prop-types';
+import {Auth} from "../../assets/Auth"
+import { useNavigate } from 'react-router-dom';
 
 export const UserMenu = props => {
     const {
         userMenuBool,
         handleUserMenu
     } = props
+    const {dispatch} = useContext(Auth)
+    const Navigate = useNavigate()
+
+    console.log({dispatch})
     const [left, setLeft] =useState(-200)
     const [opacity, setOpacity] = useState(0)
     useEffect(()=>{
@@ -24,12 +30,22 @@ export const UserMenu = props => {
     const handleIconClick =() =>{
         handleUserMenu(false)
     }
+    const logout =() =>{
+        dispatch({type: "LOGOUT", payload: null})
+        Navigate("/login")
+        handleUserMenu(false)
+    }
     return (
         <div className='userMenuCtnr' style={{
             left: left +"%",
             opacity : opacity
         }}>
             <BiSolidChevronLeft className="goBackIcon" onClick={handleIconClick}/>
+
+            <span onClick={logout}>
+            <BiLogOut className='logoutIcon'  />
+            <a>Log out</a>
+            </span>
         </div>
     )
 };
